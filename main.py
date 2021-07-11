0744b44d8258bcbf08e20946433ec6957ae2679c
Michael Zeng <michaelszeng@gmail.com>
2021-07-11 11:41:59 -0700

    added comments

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+"""
+This program takes a live videofeed from the Lepton and draws outlines around the detected person.
+
+You may have to change the cameraID depending on your setup.
+"""
+
 import cv2
 import numpy as np
 
 cameraID = 1
-videoCapture = cv2.VideoCapture(cameraID)
-# videoCapture = cv2.VideoCapture(cv2.CAP_DSHOW)
 
-if videoCapture.isOpened(): #try to get the first frame
+#Get the videofeed
+videoCapture = cv2.VideoCapture(cameraID)
+
+if videoCapture.isOpened():  #try to get the first frame in the video feed
     rval, frame = videoCapture.read()
 else:
     rval = False
@@ -17,7 +24,7 @@ while rval:     #repeatedly update the frame
 
     blurredBrightness = cv2.bilateralFilter(frame_v,9,150,150)
     thresh = 50     #threshold for counting as an edge
-    edges = cv2.Canny(blurredBrightness,thresh,thresh*2, L2gradient=True)
+    edges = cv2.Canny(blurredBrightness,thresh,thresh*2, L2gradient=True)   #edge detection
 
     _,mask = cv2.threshold(blurredBrightness,200,1,cv2.THRESH_BINARY)
     erodeSize = 5  #how much to diminish features in the image
@@ -28,6 +35,8 @@ while rval:     #repeatedly update the frame
     edges1 = cv2.resize(cv2.cvtColor(mask*edges, cv2.COLOR_GRAY2RGB), (640, 480))
     frame_with_edges = cv2.resize(cv2.cvtColor(mask*edges, cv2.COLOR_GRAY2RGB) | frame, (640, 480))     #the | character overlays the 2 frames
     # frame_with_edges = cv2.resize(mask*edges | frame_v, (640, 480))     #the | character overlays the 2 frames
+
+    #Show both the edges overlaid on the original image, and just the edges
     cv2.imshow("Edges", edges1)
     cv2.imshow("Edges Overlaid", frame_with_edges)
 
