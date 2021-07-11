11beb4a2fc541fcff8adbe78e3aa5e1d89d9fa2c
Michael Zeng <michaelszeng@gmail.com>
2021-07-11 11:35:21 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+cameraID = 1
+videoCapture = cv2.VideoCapture(cameraID)
+# videoCapture = cv2.VideoCapture(cv2.CAP_DSHOW)
+
+if videoCapture.isOpened(): #try to get the first frame
+    rval, frame = videoCapture.read()
+else:
+    rval = False
+
+while rval:     #repeatedly update the frame
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)  #convert frame to hsv
+    frame_v = frame_hsv[:,:,2]  #set V value to 2
+    # cv2.imshow("HSV Frame", frame_v)
+
+    blurredBrightness = cv2.bilateralFilter(frame_v,9,150,150)
+    thresh = 50     #threshold for counting as an edge
+    edges = cv2.Canny(blurredBrightness,thresh,thresh*2, L2gradient=True)
+
+    _,mask = cv2.threshold(blurredBrightness,200,1,cv2.THRESH_BINARY)
+    erodeSize = 5  #how much to diminish features in the image
+    dilateSize = 9  #makes remaining features more prominent
+    eroded = cv2.erode(mask, np.ones((erodeSize, erodeSize)))
+    mask = cv2.dilate(eroded, np.ones((dilateSize, dilateSize)))
+
+    edges1 = cv2.resize(cv2.cvtColor(mask*edges, cv2.COLOR_GRAY2RGB), (640, 480))
+    frame_with_edges = cv2.resize(cv2.cvtColor(mask*edges, cv2.COLOR_GRAY2RGB) | frame, (640, 480))     #the | character overlays the 2 frames
+    # frame_with_edges = cv2.resize(mask*edges | frame_v, (640, 480))     #the | character overlays the 2 frames
+    cv2.imshow("Edges", edges1)
+    cv2.imshow("Edges Overlaid", frame_with_edges)
+
+    rval, frame = videoCapture.read()
+
+    key = cv2.waitKey(20)
+    if key == 27:   #exit on ESC
+        break
